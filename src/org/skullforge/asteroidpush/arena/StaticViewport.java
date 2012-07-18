408b723868db17c0a740c3f388c55e9e1abe75f0
Christian Meyer <christian.meyer@bbv.ch>
2012-07-18 18:53:27 +0200

    Rename BasicViewport to StaticViewpart

---
+++ src/org/skullforge/asteroidpush/arena/StaticViewport.java
@@ -0,0 +1,32 @@
+package org.skullforge.asteroidpush.arena;
+
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.geom.Vector2f;
+
+public class StaticViewport implements Viewport {
+  Graphics graphicsPort;
+
+  public StaticViewport(Graphics g) {
+    graphicsPort = g;
+    graphicsPort.scale(25.0f, 25.0f);
+  }
+
+  @Override
+  public void showImage(Image img,
+                        Vector2f origin,
+                        float rotation,
+                        Vector2f size) {
+    img.setCenterOfRotation(size.getX()/2.0f, size.getY()/2.0f);
+    img.setRotation(rotation);
+    graphicsPort.drawImage(img,
+                           origin.getX(),
+                           origin.getY(),
+                           origin.getX() + size.getX(),
+                           origin.getY() + size.getY(),
+                           0.0f,
+                           0.0f,
+                           img.getWidth(),
+                           img.getHeight());
+  }
+}
---
+++ test/org/skullforge/asteroidpush/arena/StaticViewportTest.java
@@ -0,0 +1,60 @@
+package org.skullforge.asteroidpush.arena;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.Before;
+import org.junit.Test;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.geom.Vector2f;
+
+public class StaticViewportTest {
+  Mockery context;
+  Graphics graphicsMock;
+  Image imageMock;
+
+  @Before
+  public void setUp() throws Exception {
+    context = new Mockery() {
+      {
+        setImposteriser(ClassImposteriser.INSTANCE);
+      }
+    };
+    graphicsMock = context.mock(Graphics.class);
+    imageMock = context.mock(Image.class);
+  }
+
+  @Test
+  public void testMappingRectangleToDisplay() {
+    final Vector2f origin = new Vector2f(0.0f, 0.0f);
+    final float rotation = 0.0f;
+    final Vector2f size = new Vector2f(1.0f, 1.0f);
+
+    context.checking(new Expectations() {
+      {
+        allowing(imageMock).getWidth();
+        will(returnValue(50));
+        allowing(imageMock).getHeight();
+        will(returnValue(50));
+        oneOf (imageMock).setCenterOfRotation(size.x/2.0f, size.x/2.0f);
+        oneOf (imageMock).setRotation(rotation);
+        oneOf (graphicsMock).scale(25.0f, 25.0f);
+        oneOf (graphicsMock).drawImage(imageMock,
+                                    0.0f,
+                                    0.0f,
+                                    1.0f,
+                                    1.0f,
+                                    0.0f,
+                                    0.0f,
+                                    50.0f,
+                                    50.0f);
+      }
+    });
+
+    Viewport view = new StaticViewport(graphicsMock);
+    view.showImage(imageMock, origin, rotation, size);
+
+    context.assertIsSatisfied();
+  }
+}
