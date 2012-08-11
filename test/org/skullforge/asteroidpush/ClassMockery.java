d809b6fd9c419c8bbf93c628f5d8b14a41c71d44
Christian Meyer <christian.meyer@bbv.ch>
2012-08-11 19:28:05 +0200

    Add utility test class for mocking non-interface classes.

---
+++ test/org/skullforge/asteroidpush/ClassMockery.java
@@ -0,0 +1,18 @@
+package org.skullforge.asteroidpush;
+
+import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
+
+/**
+ * This is an extension of the jmock Mockery that can also mock non-interface
+ * classes. Use this instead of using the anonymous inner class definition that
+ * needlessly clutters your test code.
+ * 
+ * @author Konfuzzyus
+ * 
+ */
+public class ClassMockery extends Mockery {
+   public ClassMockery() {
+      setImposteriser(ClassImposteriser.INSTANCE);
+   }
+}
