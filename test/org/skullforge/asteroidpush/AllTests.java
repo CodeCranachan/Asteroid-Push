d40fbbf7dec3bffd9e4acbb8ff1a88d0f6795ee0
Christian Meyer <christian.meyer@bbv.ch>
2012-08-11 19:41:04 +0200

    Add minimal unit test for Scenery.

--- test/org/skullforge/asteroidpush/AllTests.java
+++ test/org/skullforge/asteroidpush/AllTests.java
@@ -5,6 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import org.skullforge.asteroidpush.arena.BasicArenaTest;
+import org.skullforge.asteroidpush.arena.entities.SceneryTest;
 import org.skullforge.asteroidpush.arena.entities.VesselTest;
 import org.skullforge.asteroidpush.parts.StaticBoxTest;
 import org.skullforge.asteroidpush.arena.viewports.StaticViewportTest;
@@ -12,7 +13,7 @@ import org.skullforge.asteroidpush.doodads.DoodadTest;
 import org.skullforge.asteroidpush.doodads.PlayingFieldBorderFactoryTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ VesselTest.class, ArenaGameStateTest.class,
+@SuiteClasses({ VesselTest.class, SceneryTest.class, ArenaGameStateTest.class,
       AsteroidPushTest.class, GameStateFactoryTest.class, BasicArenaTest.class,
       StaticViewportTest.class, StaticBoxTest.class, DoodadTest.class,
       PlayingFieldBorderFactoryTest.class, MatchGameStateTest.class,
